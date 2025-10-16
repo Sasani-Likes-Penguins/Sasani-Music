981c74373c80ae3fd38e6a5346f6d40d40cc9d32
Sasani-Likes-Penguins <sasani.music@penguinmail.com>
2025-10-16 14:41:14 -0400

    Auto updating site.

--- update.bat
+++ update.bat
@@ -1,4 +1,6 @@
 @echo off
+echo ~~~~~~~~~~~~~~~~~~~~~~~~
+echo Updating ALL the things!
+echo ~~~~~~~~~~~~~~~~~~~~~~~~
 git add -A && git commit -m "Auto updating site." && git push
-echo Updating ALL the things.
 pause
